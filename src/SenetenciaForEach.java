84d14ff59bf6fa58fa7d97a007d9f57fabfd48af
Ejercicios <mistazew@otlook.com>
2021-09-06 18:26:13 -0500

    CursoJava2021

--- src/SenetenciaForEach.java
+++ src/SenetenciaForEach.java
@@ -1,5 +1,22 @@
 public class SenetenciaForEach {
     public static void main(String[] args) {
 
+        //for
+        String caracteres = "Vega Edits";
+        char[] arreglo =  caracteres.toCharArray();
+        int incremento = arreglo.length;
+        for (int i = 0; i < incremento; i++){
+            System.out.println(arreglo[i]);
+        }
+
+        System.out.println("\n");
+
+        //forEach
+        caracteres = "Mista Zew";
+        arreglo = caracteres.toCharArray();
+        for (char caracter: arreglo){
+            System.out.println(caracter);
+        }
+
     }
 }
